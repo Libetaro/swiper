d81aca31906336dcdb44e9444a048f0296959a1a
Libetaro <77522003+Libetaro@users.noreply.github.com>
2021-01-21 14:08:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ var mySwiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 10,
   centeredSlides: true,
-  pagination: ".swiper-pagination",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
