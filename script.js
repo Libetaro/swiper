bd8a63d42e26027adf775126d88865583119a293
Libetaro <77522003+Libetaro@users.noreply.github.com>
2021-01-21 14:26:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ var mySwiper = new Swiper(".swiper-container", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
   breakpoints: {
     767: {
       slidesPerView: 1.7,
